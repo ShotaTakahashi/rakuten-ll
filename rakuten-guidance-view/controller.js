e6cb609957ccb42c7c8fa96b9ef7afcf0c14a677
lisaco-ono <lisaco.ono.ii@gmail.com>
2019-09-06 10:52:48 +0800

    天気APIからデータを取得して表示する

--- rakuten-guidance-view/controller.js
+++ rakuten-guidance-view/controller.js
@@ -1,15 +1,28 @@
-// とりあえずJavaScriptでAPI叩くときの雛形
+// // とりあえずJavaScriptでAPI叩くときの雛形
+//
+// //XMLHttpRequestの作成
+// var request = new XMLHttpRequest();
+// var KEY = "3b52a6b4f0be239a8357ec950fe1e1b9";
+//
+// //APIを叩くためのURL
+// var URL = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=3b52a6b4f0be239a8357ec950fe1e1b9";
+//
+// request.open("GET", URL, true);
+//
+// request.onload = function () {
+//     //レスポンスが返って来たときの処理
+// }
+// request.send();
 
-//XMLHttpRequestの作成
-var request = new XMLHttpRequest();
+function getWeatherFromApi() {
+    var weatherData;
+    var weatherOfThisTime;
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=3b52a6b4f0be239a8357ec950fe1e1b9",function(json){
+        weatherData = JSON.stringify(json);
 
-//APIを叩くためのURL
-var URL = "";
+        jsonWeatherData = JSON.parse(weatherData);
+        weatherOfThisTime = jsonWeatherData.weather[0].main;
 
-request.open("GET", URL, true);
-
-request.onload = function () {
-    //レスポンスが返って来たときの処理
+        document.write("It is \"" + weatherOfThisTime + "\" in London now.");
+    });
 }
-
-request.send();
