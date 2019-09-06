ff5b41957eadaf89db5a2cd86c082b83b76808d8
iszhaoxin <iszhaoxin@qq.com>
2019-09-06 11:15:49 +0900

    Revise name

--- rakuten-guidance-api/show_user/show_user.py
+++ rakuten-guidance-api/show_user/show_user.py
@@ -1,2 +1,18 @@
+import pymysql.cursors
+import json
+from pdb import set_trace
 def get_user_from_db(user_id):
-    return
+    connection = pymysql.connect(host='localhost',
+            user='zhaoxin',
+            password='sinnjiru',
+            charset='utf8mb4',
+            db='Rakuten_ll',
+            cursorclass=pymysql.cursors.DictCursor)
+
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM `%s` WHERE guidance_id=%d"%('guidance' ,user_id)
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        print(type(result))
+        print(json.(dict))
+get_user_from_db(1)
